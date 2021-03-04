7348a1adefcee776b7886522b70a896f865b78c0
Dmytro Kravtsov <dmytro.kravtsov@unicorn.com>
2021-03-04 14:38:04 +0100

    Components + CSS-Modules

--- src/App.js
+++ src/App.js
@@ -1,51 +1,14 @@
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from "./components/Profile";
 
 const App = () => {
     return (
         <div className='app-wrapper'>
-            <header className='header'>
-                <img src='https://www.logolynx.com/images/logolynx/90/90cdeb260df361a39d97540d9c75a814.png'/>
-            </header>
-            <nav className='nav'>
-                <div>
-                    <a>Profile</a>
-                </div>
-                <div>
-                    <a>Messages</a>
-                </div>
-                <div>
-                    <a>News</a>
-                </div>
-                <div>
-                    <a>Music</a>
-                </div>
-                <div>
-                    <a>Setting</a>
-                </div>
-            </nav>
-            <div className='content'>
-                <div>
-                    <img
-                        src='https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg'/>
-                </div>
-                <div>
-                    ava + description
-                </div>
-                <div>
-                    My posts
-                    <div>
-                        New post
-                    </div>
-                    <div>
-                        <div>
-                            Post 1
-                        </div>
-                        <div>
-                            Post 1
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Header/>
+            <Navbar/>
+            <Profile/>
         </div>
     );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import s from './Header.module.css';
+
+const Header = () => {
+    return (
+        <header className={s.header}>
+            <img src='https://www.logolynx.com/images/logolynx/90/90cdeb260df361a39d97540d9c75a814.png'/>
+        </header>
+    );
+}
+
+export default Header;
