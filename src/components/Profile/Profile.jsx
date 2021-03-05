a26aedab7477ab5f9e4285997f3a05b3d9ebd631
Dmytro Kravtsov <dmytro.kravtsov@unicorn.com>
2021-03-05 18:37:59 +0100

    MyPosts and ProfileInfo update.

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,25 +1,13 @@
 import React from "react";
+import ProfileInfo from './ProfileInfo/ProfileInfo';
 import MyPosts from "./MyPosts/MyPosts";
-import s from './Profile.module.css';
 
-const ProfileInfo = (props) => {
-    return (
-        <div>
-            <div>
-                <img
-                    src='https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg'/>
-            </div>
-            <div>
-                ava + description
-            </div>
-            <MyPosts/>
-        </div>
-    )
-}
+
 const Profile = () => {
     return (
         <div>
             <ProfileInfo/>
+            <MyPosts/>
         </div>
     );
 }
