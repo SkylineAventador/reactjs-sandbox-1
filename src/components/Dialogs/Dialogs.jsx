95930ffd97fa224896436d9843298884f36d08e8
Dmytro Kravtsov <dmytro.kravtsov@unicorn.com>
2021-03-05 16:22:03 +0100

    DialogItems navlinks + css update.

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
+
 
 const Dialogs = (props) => {
     return (
-      <div>
-        Dialogs
-      </div>
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}><NavLink to='/dialogs/1'>User 1</NavLink></div>
+                <div className={s.dialog}><NavLink to='/dialogs/2'>User 2</NavLink></div>
+                <div className={s.dialog}><NavLink to='/dialogs/3'>User 3</NavLink></div>
+                <div className={s.dialog}><NavLink to='/dialogs/4'>User 4</NavLink></div>
+                <div className={s.dialog}><NavLink to='/dialogs/5'>User 5</NavLink></div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.dialog}>Message 1</div>
+                <div className={s.dialog}>Message 2</div>
+                <div className={s.dialog}>Message 3</div>
+            </div>
+        </div>
     );
 }
 
